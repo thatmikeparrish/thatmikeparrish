0f4620524b89286d9498a897db74b28c404c5cf1
Mike Parrish <mparrish615@gmail.com>
2018-10-24 18:00:25 -0500

    changes to bio

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
+import Mike from "../../img/branding/mike.jpg"
+
 export default class Bio extends Component {
     render() {
         return (
             <React.Fragment>
                     <div className="content d-flex justify-content-center">
                         <h1>Bio</h1>
+                    </div>
+                    <div className="d-flex justify-content-around">
+                    <img className="mike" src={Mike} />
+                        <h4>Stuff about me</h4>
                     </div>
             </React.Fragment>
         );
