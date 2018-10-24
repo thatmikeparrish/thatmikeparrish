7e7ccbd13125d4e08858cc1182d1ac08419633ca
Mike Parrish <mparrish615@gmail.com>
2018-10-23 23:01:28 -0500

    trasnfer to laptop

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="content">
+                    <div className="container">
+                        <img src={"../../../img/branding/logoMikeParrishMix2.png"} />
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
-
-export default App;
