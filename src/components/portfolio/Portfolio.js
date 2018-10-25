4c3cf512f0c3e2f576d9d3a28be51fccace50746
Mike Parrish <40585475+mparrish615@users.noreply.github.com>
2018-10-25 13:00:56 -0500

    portfolio changes

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -14,13 +14,19 @@ export default class Portfolio extends Component {
                     <div className="portfolioEntry">
                         <img className="image" src={AmyTaylorCountry} alt="Amy Taylor Country"/>
                         <div className="overlay">
-                            <p className="text">Amy Taylor Country</p>
+                            <div className="text">
+                                <h4 className="name">Amy Taylor Country</h4>
+                                <h6 className="info">Singer Songwriter based out of Nashville</h6>
+                            </div>
                         </div>
                     </div>
                     <div className="portfolioEntry">
                         <img className="image" src={WOAH} alt="World of Apathetic Heroes"/>
                         <div className="overlay">
-                            <p className="text">World Of Apathetic Heroes</p>
+                            <div className="text">
+                                <h4 className="name"><a href="https://worldofapatheticheroes.herokuapp.com/">World Of Apathetic Heroes</a></h4>
+                                <h6 className="info">My Front-end Capstone project at NSS</h6>
+                            </div>
                         </div>
                     </div>
                 </div>
