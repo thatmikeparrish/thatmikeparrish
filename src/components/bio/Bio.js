998253d729a73b92904a5e40f9289b81ff92535b
Mike Parrish <40585475+mparrish615@users.noreply.github.com>
2018-11-06 10:02:24 -0600

    backup from laptop

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -2,23 +2,29 @@ import React, { Component } from 'react';
 
 import ExNSS from '../../img/experience/nss-logo.png'
 import PF from '../../img/experience/premierfirestop.jpg'
+import BR from '../../img/experience/br.jpg'
 import EdNSS from '../../img/education/nss-logo.png'
 import ITT from '../../img/education/itt.jpg'
 
 export default class Bio extends Component {
+
     render() {
+
+        
+
         return (
             <div className="content">
                 <div className="d-flex justify-content-center">
                     <h2 className="sectionTitle">Bio</h2>
                 </div>
                 <div className="d-flex justify-content-around">
-                    <div className="mike" alt="Its a pic of me!"></div>
+                    <div className="mike" alt="Me or things I like!"></div>
                     <div className="myBio d-flex flex-column justify-content-around">
-                        <p>Howdy, I'm Mike, and welcome to my page! I'm a huge nerd who loves the outdoors just as much as games and comics.</p>
-                        <p>Solving problems and figuring out problems is one of the best aspects of development to me. With development I can help others by providing a technical solution. I truly enjoy front-end development, with front-end I can be as creative as the project calls for.</p>
+                        <p>Howdy, my name is Mike Parrish and I am a software developer with an affinity for thinking outside the box. I'm currently attending Nashville Software School to hone my development skills.</p>
+                        <p>A little about me... I'm a huge nerd that plays games and reads comics. Flash and Green Lantern are my favorite super heroes! When I'm not working I'm typically playing World of Warcraft or Magic the Gathering. Camping and backpacking are a huge part of my life, both my fiancee and I enjoy getting out, taking the path less traveled, and exploring all that we can.</p>
                     </div>
                 </div>
+                <h6 className="devQuote">"Things aren't always #000000 and #ffffff, so let me help you decode the #808080!"</h6>
                 <h4 className="sectionSubTitle">Experience</h4>
                 <div className="experience d-flex">
                     <img className="exLogo" src={ExNSS} alt="Nashville Software School" />
@@ -59,6 +65,37 @@ export default class Bio extends Component {
                             <li>Designed and implemented a company-wide database to provide BI and company health reporting</li>
                             <li>Created and maintained client sites and user credentials in WordPress to provide access to documents required by the clients</li>
                             <li>Created and maintained an active inventory control of all IT related items within Premier Firestop</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="experience d-flex">
+                    <img className="exLogo" src={BR} alt="Premier Firestop" />
+                    <div className="exContent">
+                        <h5>Estimator II</h5>
+                        <h6>B.R. McMillan & Assoc. Inc.</h6>
+                        <h6>March 2010 - October 2015</h6>
+                        <p>A construction company specializing in commercial construction for new construction in healthcare and military. Performs general trades packages, Division 10 packages, and Firestopping packages.</p>
+                        <ul>
+                            <li>Assisted in identifying new work to be pursued that is consistent with the Company’s marketing strategy.</li>
+                            <li>Cultivated new business ventures through existing business relationships and former customers.</li>
+                            <li>Analyzed job site conditions as necessary and prepared takeoff of trades performed by B.R. McMillan.</li>
+                            <li>Generated request for information forms (RFI’s) where appropriate to provide quality assurance and customer satisfaction.</li>
+                            <li>Ensured that estimates are priced with an estimated gross profit consistent with the necessary profit objectives of B.R McMillan as communicated by senior management.</li>
+                            <li>Managed and maintained both technical drawings and specifications for all assigned projects. </li>
+                            <li>Prepared quotes for future business endeavors including budgeting/estimating, and submission for all trades contracted by B.R. McMillan. </li>
+                            <li>Oversaw and negotiated newly awarded contracts for B.R. McMillan.</li>
+                            <li>Established a schedule of values for each project and ensured that field reporting forms were consistent with the estimate.</li>
+                            <li>Responsible for monthly billings for assigned projects.</li>
+                            <li>Developed and implemented an in-depth training program for both current estimators and new hires.</li>
+                            <li>Optimized the excel based estimating software used by the Estimating department.</li>
+                        </ul>
+                        <h6>IT Systems and SharePoint Development Manager</h6>
+                        <ul>
+                            <li>Enhanced the day to day workflow at B.R McMillian by developing and implementing the utilization of Office 365 and SharePoint.</li>
+                            <li>Established and deployed the use of an IT ticket service platform to document IT related issues within B.R. McMillan.</li>
+                            <li>Provided training and technical support to users of applications such as On-Screen Takeoff, SharePoint, network, ShoreTel, and server issues through IT ticket service.</li>
+                            <li>Configured and managed Microsoft Office 365 and SharePoint user accounts.</li>
+                            <li>Constructed, diagnosed, and maintained computer hardware utilized by B.R. McMillan.</li>
                         </ul>
                     </div>
                 </div>
