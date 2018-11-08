5a18c32926f7bab5500636ab190dbcbbf34f428b
Mike Parrish <40585475+mparrish615@users.noreply.github.com>
2018-11-08 15:05:39 -0600

    logos

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,11 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import MainLogo from "../../img/branding/logoMikeParrishMix2.png"
 
-import NavBar from '../navbar/Nav'
-import Bio from "../bio/Bio"
-import Contact from "../contact/Contact"
-import Portfolio from "../portfolio/Portfolio"
-import Reviews from "../reviews/Reviews"
+import NavBar from '../Nav/Nav'
+import Bio from "../Bio/Bio"
+import Contact from "../Contact/Contact"
+import Portfolio from "../Portfolio/Portfolio"
+import Reviews from "../Reviews/Reviews"
 
 export default class App extends Component {
     render() {
